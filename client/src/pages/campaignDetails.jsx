bbffb52a74b04167163172d10536172f35724d60
Ketan-Kandola <c0006267@newcastle.ac.uk>
2023-03-27 13:30:32 +0100

    forgot to include campaign withdrawal

---
+++ client/src/pages/campaignDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const campaignDetails = () => {
+  return (
+    <div>campaignDetails</div>
+  )
+}
+
+export default campaignDetails
---
+++ client/src/pages/campaignWithdrawal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const campaignWithdrawals = () => {
+  return (
+    <div>campaignWithdrawals</div>
+  )
+}
+
+export default campaignWithdrawals
