307979bf4fc7cfb0b5a822a9b5ef1270dec8bb95
Ketan-Kandola <c0006267@newcastle.ac.uk>
2023-03-30 13:21:16 +0100

    start creation of campaign form

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,16 +1,41 @@
 //aim to make the page responsive for small screens hence when creating the page include padding for smaller margins
 
 import React, {useState} from 'react'
-/* import { useNavigate } from 'react-router-dom';
-import { ethers } from 'ethers';
-import { CustomButton, FormField, Loader } from '../components';
-//import { useStateContext } from '../context';
+import {useNavigate} from 'react-router-dom';
+import {ethers} from 'ethers';
+
 import { checkIfImage } from '../utils';
- */
+import {CustomButton, CreateCampaignForm} from '../components';
+
 const CreateCampaign = () => {
-    // const
+    const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
+    const [form, setForm] = useState({
+        name: '',
+        title: '',
+        description: '',
+        targetGoal: '', 
+        deadline: '',
+        imageUrl: ''
+    });
+
+    const handleSubmit = () => {};
+
     return (
-        <div className="py-24">CreateCampaign</div>
+        <div className="flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+            {isLoading && 'loader'}
+            <div className="flex justify-center items center py-24 px-6">
+                <h1 className="font-epilogue font-light sm:text-[61px] text-[48px] leading-[38px] text-[#0D0D0D]">Start your Campaign</h1>
+            </div>
+
+            <form onSubmit={handleSubmit} className="w-full mt-[65px] flex flex-col gap-[30px]">
+                <div className="flex flex-wrap gap-[40px]">
+                    <CreateCampaignForm />
+                </div>
+
+            </form>
+            
+        </div>
     )
 }
 export default CreateCampaign
