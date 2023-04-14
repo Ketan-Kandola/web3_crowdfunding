cbee4fc5dc927c69ca68d9e168c99174f30b76e1
Ketan-Kandola <c0006267@newcastle.ac.uk>
2023-04-14 12:48:12 +0100

    tests

--- test/donation.test.js
+++ test/donation.test.js
@@ -1,129 +1,170 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
 
-describe("Campaign contract", function () {
-    let CampaignContract;
-    let campaign;
-    let owner;
-    let campaignTarget;
-    let campaignDeadline;
-    let campaignTitle;
-    let campaignDescription;
-    let imageForCampaign;
+describe("Donation contract", function() {
+    let donation;
+    let campaignID;
 
-    beforeEach(async () => {
-        // Get the Campaign contract
-        CampaignContract = await ethers.getContractFactory("Campaign");
-
-        // Deploy a new campaign contract
-        campaign = await CampaignContract.deploy();
-
-        // Get the owner address
-        [owner] = await ethers.getSigners();
-
-        // Set the campaign target amount and deadline
-        campaignTarget = ethers.utils.parseEther("10");
-        campaignDeadline = Math.floor(Date.now() / 1000) + 3600; // 1 hour from now
-
-        // Set the campaign title, description, and image URL
-        campaignTitle = "Test Campaign";
-        campaignDescription = "This is a test campaign";
-        imageForCampaign = "https://example.com/test.jpg";
+    before(async function() {
+    const Donation = await ethers.getContractFactory("Donation");
+    donation = await Donation.deploy();
+    await donation.deployed();
     });
 
-    it("should create a new campaign", async function () {
-        // Create a new campaign
-        const tx = await campaign.createCampaign(
-        owner.address,
-        campaignTitle,
-        campaignDescription,
-        campaignTarget,
-        campaignDeadline,
-        imageForCampaign
-        );
-
-        // Get the ID of the new campaign
-        const campaignId = tx.receipt.logs[0].args[0].toNumber();
-
-        // Get the details of the new campaign
-        const result = await campaign.campaigns(campaignId);
-
-        // Check that the campaign details are correct
-        expect(result.creatorAddress).to.equal(owner.address);
-        expect(result.title).to.equal(campaignTitle);
-        expect(result.description).to.equal(campaignDescription);
-        expect(result.goalAmount).to.equal(campaignTarget);
-        expect(result.deadline).to.equal(campaignDeadline);
-        expect(result.amountCollected).to.equal(0);
-        expect(result.imageURL).to.equal(imageForCampaign);
+    describe("createCampaign", function() {
+        it("should create a new campaign", async function() {
+            const campaignTitle = "Test Campaign";
+            const campaignDescription = "This is a test campaign";
+            const campaignTarget = ethers.utils.parseEther("1");
+            const campaignDeadline = Math.floor(Date.now() / 1000) + 3600;
+            const imageForCampaign = "https://static.independent.co.uk/s3fs-public/thumbnails/image/2020/04/06/00/stay-home-veg-box.jpg";
+            const ownerAddress = await ethers.getSigner(0).getAddress();
+            campaignID = await donation.createCampaign(ownerAddress, campaignTitle, campaignDescription, campaignTarget, campaignDeadline, imageForCampaign);
+            expect(campaignID).to.equal(0);
+        });
     });
 
-    it("should not create a new campaign with an empty title", async function () {
-        // Try to create a new campaign with an empty title
-        await expect(
-        campaign.createCampaign(
-            owner.address,
-            "",
-            campaignDescription,
-            campaignTarget,
-            campaignDeadline,
-            imageForCampaign
-        )
-        ).to.be.revertedWith("Title cannont be empty");
+    describe("donateToCampaign", function() {
+        it("should donate to the campaign", async function() {
+            const donationAmount = ethers.utils.parseEther("0.5");
+            await donation.connect(ethers.provider.getSigner(1)).donateToCampaign(campaignID, { value: donationAmount });
+            const [contributors, donations] = await donation.getDonators(campaignID);
+            expect(contributors.length).to.equal(1);
+            expect(donations.length).to.equal(1);
+            expect(donations[0]).to.equal(donationAmount);
+        });
+        it("should refund all contributors if deadline has passed", async function() {
+            const donationAmount = ethers.utils.parseEther("0.5");
+            const campaign = await donation.campaigns(campaignID);
+            const deadline = campaign.deadline;
+            await ethers.provider.send("evm_setNextBlockTimestamp", [deadline+1]);
+            await ethers.provider.send("evm_mine");
+
+            const initialContractBalance = await ethers.provider.getBalance(donation.address);
+            const initialDonorBalance = await ethers.provider.getBalance(await ethers.provider.getSigner(1).getAddress());
+
+            await donation.connect(ethers.provider.getSigner(1)).donateToCampaign(campaignID, { value: donationAmount });
+
+            const finalContractBalance = await ethers.provider.getBalance(donation.address);
+            const finalDonorBalance = await ethers.provider.getBalance(await ethers.provider.getSigner(1).getAddress());
+
+            expect(finalContractBalance).to.equal(initialContractBalance);
+            expect(finalDonorBalance).to.be.above(initialDonorBalance);
+        });
     });
 
-    it("should not create a new campaign with an empty description", async function () {
-        // Try to create a new campaign with an empty description
-        await expect(
-        campaign.createCampaign(
-            owner.address,
-            campaignTitle,
-            "",
-            campaignTarget,
-            campaignDeadline,
-            imageForCampaign
-        )
-        ).to.be.revertedWith("Description cannont be empty");
+    describe("updateCampaign", function() {
+        it("should update the campaign", async function() {
+            const campaignTitle = "Updated Campaign";
+            const campaignDescription = "This is an updated campaign";
+            const imageForCampaign = "https://example.com/updated.png";
+            const result = await donation.updateCampaign(campaignID, campaignTitle, campaignDescription, imageForCampaign);
+            expect(result).to.be.true;
+            const campaign = await donation.campaigns(campaignID);
+            expect(campaign.title).to.equal(campaignTitle);
+            expect(campaign.description).to.equal(campaignDescription);
+            expect(campaign.imageURL).to.equal(imageForCampaign);
+        });
     });
 
-    it("should not create a new campaign with an empty image URL", async function () {
-        // Try to create a new campaign with an empty image URL
-        await expect(
-        campaign.createCampaign(
-            owner.address,
-            campaignTitle,
-            campaignDescription,
-            campaignTarget,
-            campaignDeadline,
-            ""
-        )
-        ).to.be.revertedWith("Image url cannont be empty");
+    describe("removeCampaign", function() {
+        it("should remove the campaign", async function() {
+            const result = await donation.removeCampaign(campaignID);
+            expect(result).to.be.true;
+            const campaign = await donation.campaigns(campaignID);
+            expect(campaign.status).to.equal(3); // campaignStatus.removed
+        });
+    });
+    
+    describe("refund", function() {
+        it("should refund all contributors for a campaign", async function() {
+            // Create a new campaign
+            const campaignId = await donation.createCampaign(ownerAddress, "Campaign Title", "Campaign Description", web3.utils.toWei("1"), Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30, "https://static.independent.co.uk/s3fs-public/thumbnails/image/2020/04/06/00/stay-home-veg-box.jpg");
+    
+            // Donate to the campaign
+            await donation.donateToCampaign(campaignId, { from: donor1Address, value: web3.utils.toWei("0.5") });
+            await donation.donateToCampaign(campaignId, { from: donor2Address, value: web3.utils.toWei("0.3") });
+            await donation.donateToCampaign(campaignId, { from: donor3Address, value: web3.utils.toWei("0.2") });
+    
+            // Refund all contributors for the campaign
+            await donation.refund(campaignId);
+    
+            // Check that all contributors were refunded
+            const supporter1 = await donation.supporterOfCampaign(campaignId, 0);
+            assert.strictEqual(supporter1.refunded, true, "Contributor 1 was not refunded");
+            const supporter2 = await donation.supporterOfCampaign(campaignId, 1);
+            assert.strictEqual(supporter2.refunded, true, "Contributor 2 was not refunded");
+            const supporter3 = await donation.supporterOfCampaign(campaignId, 2);
+            assert.strictEqual(supporter3.refunded, true, "Contributor 3 was not refunded");
+        });
     });
 
-    it("should not create a new campaign with a target amount of zero", async function () {
-        // Try to create a new campaign with a target amount of zero
-        await expect(
-        campaign.createCampaign(
-            owner.address,
-            campaignTitle,
-            campaignDescription,
-            0,
-            campaignDeadline,
-            imageForCampaign
-        )
-        ).to.be.revertedWith("The target amount should be greater than zero");
-    })
-    it("should not create a new campaign with a deadline in the past", async function () {
-        // Try to create a new campaign with a deadline in the past
-        await expect(
-            campaign.createCampaign(
-            owner.address,
-            campaignTitle,
-            campaignDescription,
-            campaignTarget,
-            Math.floor(Date.now() / 1000) - 3600, // 1 hour ago
-            imageForCampaign
-            )
-        ).to.be.revertedWith("The deadline should be in the future");
+    describe("payTo", function() {
+        it("should transfer ether to an address", async function() {
+            // Send ether to the contract
+            await web3.eth.sendTransaction({ from: donor1Address, to: donation.address, value: web3.utils.toWei("1") });
+    
+            // Transfer the ether to the recipient
+            const recipientBalanceBefore = await web3.eth.getBalance(recipientAddress);
+            await donation.payTo(recipientAddress, web3.utils.toWei("0.5"));
+            const recipientBalanceAfter = await web3.eth.getBalance(recipientAddress);
+    
+            // Check that the recipient's balance was increased by the transferred amount
+            assert.strictEqual(recipientBalanceAfter - recipientBalanceBefore, web3.utils.toWei("0.5"), "Recipient's balance was not increased by the transferred amount");
+        });
     });
-})
+
+    describe("createWithdrawalRequest", function() {
+        it("should create a new withdrawal request", async function() {
+            const campaignId = campaignID;
+            const requestDescription = "Withdraw Request";
+            const requestAmount = ethers.utils.parseEther("0.5");
+            const recipient = await ethers.getSigner(0).getAddress();
+            const requestId = await donation.createWithdrawalRequest(campaignId, requestDescription, requestAmount, recipient);
+            expect(requestId).to.equal(0);
+        });
+    });
+    describe("voteWithdrawRequest", function() {
+        it("should allow a user to vote on a withdrawal request", async function() {
+            const requestId = 0;
+            const result = await donation.voteWithdrawRequest(requestId);
+            expect(result).to.be.true;
+            const withdrawRequest = await donation.campaignWithdrawRequests(requestId);
+            expect(withdrawRequest.noOfVotes).to.equal(1);
+        });
+    });
+    describe("withdrawRequestedAmount", function () {
+        it("should withdraw funds from a campaign", async function () {
+            const campaignId = await donation.createCampaign(ownerAddress, "Campaign Title", "Campaign Description", web3.utils.toWei("1"), Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30, "https://static.independent.co.uk/s3fs-public/thumbnails/image/2020/04/06/00/stay-home-veg-box.jpg");
+
+            // Donate to the campaign
+            await donation.donateToCampaign(campaignId, { from: donor1Address, value: web3.utils.toWei("0.5") });
+            await donation.donateToCampaign(campaignId, { from: donor2Address, value: web3.utils.toWei("0.3") });
+            await donation.donateToCampaign(campaignId, { from: donor3Address, value: web3.utils.toWei("0.2") });
+
+            // Create a campaign withdrawal request
+            const requestDescription = "Withdraw Request";
+            const requestAmount = web3.utils.toWei("0.9");
+            const recipient = ownerAddress;
+            const requestId = await donation.createWithdrawalRequest(campaignId, requestDescription, requestAmount, recipient, { from: ownerAddress });
+
+            // Vote on the withdrawal request
+            await donation.voteWithdrawRequest(requestId, { from: donor1Address });
+            await donation.voteWithdrawRequest(requestId, { from: donor2Address });
+
+            // Withdraw the requested amount
+            await donation.withdrawRequestedAmount(requestId, campaignId, { from: ownerAddress });
+
+            // Check that the withdrawal request was marked as completed
+            const withdrawalRequest = await donation.campaignWithdrawRequests(requestId);
+            assert.strictEqual(withdrawalRequest.isCompleted, true, "Withdrawal request was not marked as completed");
+
+            // Check that the recipient received the requested amount
+            const recipientBalance = await web3.eth.getBalance(recipient);
+            assert.strictEqual(recipientBalance.toString(), requestAmount, "Recipient did not receive the requested amount");
+
+            // Check that the contract's balance was reduced by the requested amount
+            const contractBalance = await web3.eth.getBalance(donation.address);
+            assert.strictEqual(contractBalance.toString(), web3.utils.toWei("0.1"), "Contract balance was not reduced by the requested amount");
+        });
+    });
+});
